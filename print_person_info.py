150957fdf117f7b73dd12877489b17552aca19d0
khalidfurmulie <kfurmulie@student.rtc.edu>
2022-05-10 13:39:49 -0700

    Create print_person_info.py

---
+++ print_person_info.py
@@ -0,0 +1,20 @@
+# My Name:
+
+# write your code here
+def print_person_info(name, age, weight, height):
+
+      print(f"Name: {name}")
+
+      print(f"Age: {age}")
+
+      print(f"Weight: {weight}")
+
+      print(f"Height: {height//12} ft, {height%12} inches")
+
+      print()
+
+print_person_info("Joe", 22, 123, 69)
+
+print_person_info("Josh", 32, 156, 74)
+
+print_person_info("Mabel", 21, 106, 62)
